3c5fcb8a733c92af382c351a605e47021d9835c9
Stuart Leitch <Toruitas@gmail.com>
2019-11-03 11:59:00 +0000

    Initial commit for Thinking Cap Project, with week 1 included

---
+++ README.md
@@ -0,0 +1,29 @@
+# Physical Computing Final Project Log - Week 1
+Msc Creative Computing 2019
+Physical Computing 1 w/ Phoenix Perry
+By Stuart Leitch
+
+## Background
+
+The "Thinking Cap" is meant to be an attention-preservation device. It is a brain-computer-interface that monitors brain waves, and notes when there are periods of attention on one thing, or this attention changes to something else.
+
+The Thinking Cap is based off of a Muse 2014 headset, at this point. 
+
+## Week 1 (2019/10/28 - 2019/11/3):
+### Challenge: Get Arduino and Muse talking to each other.
+1. + Received the Muse EEG wearable from Sheldon.
+![Muse](https://github.com/Toruitas/thinkingcap/blob/master/img/wk1/muse2014.JPG)
+2. + Charged the Muse and tested it using Muse's iOS app while working on something. There was a 5-minute meditation exercise as introduction to the hardware. Rather than meditate, I did some browsing online. Flipping through tabs and following links. Reviewing the brain wave graph after the conclusion of the 5-minute intro exercise showed I was mainly in the "calm" section which supposedly indicates focus.
+3. - Attempted to link the Muse with my computer using the open source muse-lsl package. Failed because is only supports the Muse 2016 and later models. This model is in fact a 2014 model. ([muse-lsl GitHub repo](https://github.com/alexandrebarachant/muse-lsl))
+4. - Muse doesn't support or even provide their SDK anymore, except in special situations. The documentations site is down. Emailed them re: this project.
+5. - Muse responded and didn't deign to make the SDK available for my research.
+6. + Nothing on the internet can ever truly disappear. Found the last version of the SDKs from 2015 for Linux and Windows, along with old versions of the documentation.
+7. - Windows version doesn't work, since it was pre-Windows 10. It will just stop receiving data without throwing an error.
+![Windows fail](https://github.com/Toruitas/thinkingcap/blob/master/img/wk1/windows-sdk-fail.JPG)
+8. + Linux SDK version still works. 
+9. - Arduino won't talk to Linux at all. Sometimes it will connect for a mere moment, but then disappear from the ports.
+
+## Week 2 (2019/11/4 - 2019/11/10):
+### Challenge: Get Linux to recognize the Arduino, so the Muse and Arduino can pass info. Getting Windows to talk to the Muse seems a more difficult task.
+
+
---
+++ oscserver.py
@@ -0,0 +1,73 @@
+"""Small example OSC server
+ 
+This program listens to several addresses, and prints some information about
+received packets.
+"""
+import argparse
+import math
+import numpy as np
+import pyeeg
+ 
+from pythonosc import dispatcher
+from pythonosc import osc_server
+from pythonosc import udp_client
+ 
+def normalize(L,trucs):
+    for i in range(4):
+        L[i] = (L[i]-trucs[i][0])/trucs[i][1]
+    return L
+ 
+def compute_baseline(signal, time, newData):
+    if signal.shape[0] <= (time*220):
+        signal = np.vstack((signal,newData))
+    if signal.shape[0] == (time*220): #une minute de eeg
+        print("creating baseline...")
+        for i in range(4):
+            baseline.append([np.mean(signal[:,i]),np.std(signal[:,i])])
+        print(baselineSet)
+        print(signal.shape)
+        signal = np.array([]).reshape(0,4)
+        print(signal.shape)
+        print('Done')
+    return signal
+ 
+def handler(unused_addr, args, ch1,ch2,ch3,ch4):
+    baseline = args[1] # on recupere la baseline
+    baselineSet = len(baseline)==0
+    L= [ch1,ch2,ch3,ch4]
+    if baselineSet:
+        args[0]=compute_baseline(args[0],10 ,L)
+ 
+    if not baselineSet:
+        args[0] = np.vstack((args[0],normalize(L, baseline))) #On ajoute les nouvelles valeurs EEG
+        data = args[0]
+        buf = np.mean(data[-3*220:], axis=1)
+        print(data.shape)
+        value = pyeeg.dfa(buf.ravel())
+        client.send_message("/puredata/dfa", value)
+ 
+if __name__ == "__main__":
+ 
+  Parser = argparse.ArgumentParser()
+  Parser.add_argument("--clientip", default="192.168.0.199",
+      help="The ip of the OSC server")
+  Parser.add_argument("--clientport", type=int, default=5005,
+      help="The port the OSC server is listening on")
+  Parser.add_argument("--serverip",
+      default="127.0.0.1", help="The ip to listen on")
+  Parser.add_argument("--serverport",
+      type=int, default=5000, help="The port to listen on")
+  Args = Parser.parse_args()
+ 
+  client = udp_client.SimpleUDPClient(Args.clientip, Args.clientport)
+ 
+  baseline = []
+  L = np.array([]).reshape(0,4)
+  dispatcher = dispatcher.Dispatcher()
+  #dispatcher.map("/muse/eeg", print)
+  dispatcher.map("/muse/eeg", handler, L, baseline)
+ 
+  server = osc_server.ThreadingOSCUDPServer(
+      (Args.serverip, Args.serverport), dispatcher)
+  print("Serving on {}".format(server.server_address))
+  server.serve_forever()
