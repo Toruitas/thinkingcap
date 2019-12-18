1c1ac3191cf0f37de651f646cadd2052f095e35f
Stuart Leitch <toruitas@gmail.com>
2019-12-18 00:46:34 +0000

    Successfully dangerously sharing context between the threads

--- local_connection_serial.py
+++ local_connection_serial.py
@@ -27,6 +27,7 @@ last_reading = ContextVar("last_reading", default=datetime.datetime.now())
 attention_lvl = ContextVar("attn", default=0.0)  # for storing the current level of attention
 
 
+
 def eeg_handler(unused_addr, args, ch1, ch2, ch3, ch4):
     """
     Handles raw electrical data handed over from the 4 electrodes. Not needed in current version.
@@ -41,7 +42,7 @@ def eeg_handler(unused_addr, args, ch1, ch2, ch3, ch4):
     print("EEG (uV) per channel: ", ch1, ch2, ch3, ch4)
 
 
-def concentration_handler(unused_addr, args, concentration):
+def concentration_handler(unused_addr:str, fixed_argument, concentration):
     """
     This is called every time data comes in by the OSC dispatcher.
     :param unused_addr: the given address that's associated with this "view"
@@ -49,16 +50,32 @@ def concentration_handler(unused_addr, args, concentration):
     :param concentration: values of concentration
     :return:
     """
+    # print("Current attention level: ", concentration)
+    # if concentration >= 0.5 and mentally_focused.get() is False:
+    #     mentally_focused.set(True)
+    #     if not user_override.get():
+    #         focused.set(mentally_focused.get())
+    #     print("Focused, you bad mofo, you. Look at that.")
+    #     return mentally_focused.get()
+    # else:
+    #     mentally_focused.set(False)
+    #     if not user_override.get():
+    #         focused.set(mentally_focused.get())
+    #     return mentally_focused.get()
+    a_s = fixed_argument[0]
+
     print("Current attention level: ", concentration)
-    if concentration >= 0.5 and mentally_focused.get() is False:
-        mentally_focused.set(True)
-        if not user_override.get():
-            focused.set(True)
+    if concentration >= 0.5:
+        a_s.mentally_focused = True
+        if not a_s.user_override:
+            a_s.focused = a_s.mentally_focused
         print("Focused, you bad mofo, you. Look at that.")
+        # return a_s.mentally_focused
     else:
-        mentally_focused.set(False)
-        if not user_override.get():
-            focused.set(False)
+        a_s.mentally_focused = False
+        if not a_s.user_override:
+            a_s.focused = a_s.mentally_focused
+        # return a_s.mentally_focused
 
 
 async def loop1():
@@ -67,9 +84,10 @@ async def loop1():
         await asyncio.sleep(1)
 
 
-async def loop(ser):
+async def loop(ser, async_state):
 
-    while hat_running.get():
+    while async_state.hat_running:
+    # while hat_running.get():
         # user_input = input('Test control of Arduino? f for focus; anything else to quit.')
         #
         # if user_input == 'f':
@@ -85,13 +103,13 @@ async def loop(ser):
         state = ser.readline()
         if state:
             # connection made, update the variables
-            update_server_state(state)
+            update_server_state(state, async_state)
         else:
-            update_client_state(ser)
+            update_client_state(ser, async_state)
         await asyncio.sleep(.1)
 
 
-def update_server_state(state: str):
+def update_server_state(state: str, async_state):
     """
     Takes a new state from the Ard and mirrors it on this server.
     Uses set.state since we're in async land
@@ -106,15 +124,21 @@ def update_server_state(state: str):
         return False  # early return to prevent the rest of the fn from running
 
     if type(state) == dict:
-        hat_running.set(True)
-        connected.set(True)
+        # hat_running.set(True)
+        # connected.set(True)
+        # focused.set(state["focused"])
+        # wearing.set(state["wearing"])
+        # user_override.set(state["userOverride"])
+        # last_reading.set(datetime.datetime.now())
 
-        focused.set(state["focused"])
-        wearing.set(state["wearing"])
-        user_override.set(state["userOverride"])
-        last_reading.set(datetime.datetime.now())
+        async_state.hat_running = True
+        async_state.connected = True
+        async_state.focused = state["focused"]
+        async_state.wearing = state["wearing"]
+        async_state.user_override = state["userOverride"]
+        async_state.last_reading = datetime.datetime.now()
 
-        state_dict = context_vars_to_state_dict()
+        state_dict = context_vars_to_state_dict(async_state)
 
         # log the update to the console
         print("Server state updated to: ")
@@ -126,13 +150,13 @@ def update_server_state(state: str):
         print(state)
 
 
-def update_client_state(ser):
+def update_client_state(ser, async_state):
     """
     Update the Arduino client.
     https://stackoverflow.com/questions/22275079/pyserial-write-wont-take-my-string
     :return:
     """
-    state = context_vars_to_state_dict()
+    state = context_vars_to_state_dict(async_state)
     state_json = json.dumps(state)  # dump to a JSON string
 
     ser.write(state_json.encode())  # encode
@@ -141,26 +165,36 @@ def update_client_state(ser):
     print(state_json)
 
 
-def context_vars_to_state_dict() -> dict:
+def context_vars_to_state_dict(async_state) -> dict:
     """
     Convenience fn to turn the context variables into a dictionary,
     :return:
     """
     # print(mentally_focused.get())
+    # state_dict = {
+    #     "focused": focused.get(),
+    #     "mentally_focused": mentally_focused.get(),
+    #     "wearing": wearing.get(),
+    #     "hat_running": hat_running.get(),
+    #     "connected": connected.get(),
+    #     "user_override": user_override.get(),
+    #     "last_reading": last_reading.get()
+    # }
+
     state_dict = {
-        "focused": focused.get(),
-        "mentally_focused": mentally_focused.get(),
-        "wearing": wearing.get(),
-        "hat_running": hat_running.get(),
-        "connected": connected.get(),
-        "user_override": user_override.get(),
-        "last_reading": last_reading.get()
+        "focused": async_state.focused,
+        "mentally_focused": async_state.mentally_focused,
+        "wearing": async_state.wearing,
+        "hat_running": async_state.hat_running,
+        "connected": async_state.connected,
+        "user_override": async_state.user_override,
+        "last_reading": async_state.last_reading
     }
 
     return state_dict
 
 
-def make_connection(ser):
+def make_connection(ser, async_state):
     """
     Using the given serial connection, attempts to start reading data. If it tries long enough, it'll time out.
     :param ser:
@@ -171,7 +205,7 @@ def make_connection(ser):
         state = ser.readline()
         if state:
             # connection made, update the variables
-            updated = update_server_state(state)
+            updated = update_server_state(state, async_state)
             if updated:
                 print("Connected to Thinking Cap. Don't let your dreams be dreams!")
             return True
@@ -186,24 +220,36 @@ async def init_main():
 
     :return:
     """
+    # making a super ghetto state to share b/w the two async contexts
+    async_state = type('', (), {})()
+    async_state.focused = False
+    async_state.mentally_focused = False
+    async_state.wearing = False
+    async_state.hat_running = False
+    async_state.user_override = False
+    async_state.last_reading = datetime.datetime.now()
+    async_state.attention_lvl = 0.0
+
     # Prepare the serial port
     # make_connection
     ser = serial.Serial(ARD_PORT, baudrate=9600, timeout=1)
-    connection_made = make_connection(ser)
+    connection_made = make_connection(ser, async_state)
 
     if connection_made:
         # with the connection made and inital state set, can start the main loops.
+        event_loop_local = asyncio.get_event_loop()
         dispatcher = Dispatcher()
-        dispatcher.map("/muse/elements/experimental/concentration", concentration_handler, "Concentration")
+        dispatcher.map("/muse/elements/experimental/concentration", concentration_handler, async_state)
         # creates an OSC server that's Async.
-        server = osc_server.AsyncIOOSCUDPServer((IP, OSC_PORT), dispatcher, asyncio.get_event_loop())
+        server = osc_server.AsyncIOOSCUDPServer((IP, OSC_PORT), dispatcher, event_loop_local)
         transport, protocol = await server.create_serve_endpoint()  # Create datagram endpoint and start serving
 
-        await loop(ser)  # Enter main loop of program.
+        await loop(ser, async_state)  # Enter main loop of program.
 
         transport.close()  # Clean up serve endpoint
 
-
+event_loop = asyncio.new_event_loop()
+asyncio.set_event_loop(event_loop)
 asyncio.run(init_main())
 
 
